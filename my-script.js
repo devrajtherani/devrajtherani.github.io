391b0737c6d2e04851be2535cc762480001ceb0f
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2021-05-10 22:42:40 +0530

    my-script.js

--- my-script.js
+++ my-script.js
@@ -1,10 +1,15 @@
 var myVar;
 
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
+function myFunction(){
+    myVar = setTimeout(showPage, 3000);
 }
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+function showPage(){
+    document.getElementById("loader") .style.display = "none";
+    document.getElementById("myDiv") .style.display = "block";
+}
+function openNav(){
+    document.getElementById("mySidenav") .style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav") .style.width = "0";
 }
