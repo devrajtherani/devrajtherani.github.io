3a948e8309053106ebb41519f711a66965c77294
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2021-09-17 19:38:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,6 +60,11 @@ var x = setInterval(function() {
     x = setTimeout(countdown, 5000);
   }
 }, 1000);
-   function onSubmit(token) {
-     document.getElementById("demo-form").submit();
-   }
+      function onClick(e) {
+        e.preventDefault();
+        grecaptcha.ready(function() {
+          grecaptcha.execute('reCAPTCHA_site_key', {action: 'submit'}).then(function(token) {
+              // Add your logic to submit to your backend server here.
+          });
+        });
+      }
