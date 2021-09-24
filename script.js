e7c77bd9409a7bd1dbdd839bd3423eb8ab8c4402
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2021-09-24 15:23:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 var myVar;
+var g;
+var d;
 
 function myFunction(){
     myVar = setTimeout(showPage, 1500);
     document.getElementById("loader-wrapper") .style.display = "block";
+    document.getElementById("share") .style.display = "none";
+    document.getElementById("copied").style.display = "none";
 }
 
 function showPage(){
     document.getElementById("loader-wrapper") .style.display = "none";
     document.getElementById("content") .style.display = "block";
+    g = setTimeout(share, 120000)
+}
+function share(){
+  document.getElementById("content") .style.display = "none";
+  document.getElementById("share") .style.display = "block";
 }
 function openNav() {
     document.getElementById("mySidenav").style.width = "20vw";
@@ -15,5 +24,20 @@ function openNav() {
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+  }
+  function copyToClipboard(text) {
+  var inputc = document.body.appendChild(document.createElement("input"));
+  inputc.value = window.location.href;
+  inputc.focus();
+  inputc.select();
+  document.execCommand('copy');
+  inputc.parentNode.removeChild(inputc);
+  document.getElementById("copy").style.display = "none";
+  document.getElementById("copied").style.display = "block";
+  d = setTimeout(reappear, 900);
+  }
+  function reappear(){
+    document.getElementById("copy").style.display = "block";
+    document.getElementById("copied").style.display = "none";
   }
 
