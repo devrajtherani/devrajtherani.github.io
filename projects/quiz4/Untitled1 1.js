8d657038a51c0e6a18d39f2d5c87b4b93f74638d
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2025-04-08 11:33:26 +0530

    Delete projects/quiz4 directory

--- projects/quiz4/Untitled1 1.js
+++
@@ -1,9 +0,0 @@
-var myVar;
-
-function myFunction(){
-    myVar = setTimeout(showPage, 3500);
-}
-function showPage(){
-    document.getElementById("loader-wrapper") .style.display = "none";
-    document.getElementById("content") .style.display = "block";
-}
